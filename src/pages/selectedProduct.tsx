3142a5fd327bfb8218a833b20fa649a6fed7b497
george1994grandmaster <gkiladze1994@gmail.com>
2023-09-15 01:34:25 +0400

    dfdf

--- src/pages/selectedProduct.tsx
+++ src/pages/selectedProduct.tsx
@@ -64,33 +64,7 @@ const SelectProduct: FC = () => {
                 </StyledTypography>
               </Link>
             </div>
-            <div className="d-flex jc-between">
-              <div className="xs-12 sm-12 md-7">
-                <SliderOptional sliderName="selectedProductSlider" sliderParams={products[0].variations}/>
-              </div>
-              <div className="xs-12 sm-12 md-4">
-                <div className="d-flex fd-column" style={{gap: "20px"}}>
-                  <StyledTypography color="rgba(0, 0, 0, 0.88)" variant="h3" fontSize="24px" fontWeight="600">
-                    {products[0].name}
-                  </StyledTypography>
-                  <StyledTypography color="rgba(0, 0, 0, 0.88)" variant="h3" fontSize="24px" fontWeight="600">
-                    Price:&nbsp;&nbsp;${products[0].price}
-                  </StyledTypography>
-                  <div className="btn-content">
-                    <Button onClick={() => addProductHandler(products[0])} text="Add to bag" innerSpacing="10px 8px" bgColor="#00381f" color="#fff"/>
-                  </div>
-                    {isInCart ? (
-                      <div className="btn-content">
-                        <Button onClick={() => removeCartHandler(products[0].id)} text="Remove from Bag" innerSpacing="10px 8px" bgColor="#484848" color="#fff"/>
-                      </div>
-                    ) : (
-                    <div className="btn-content">
-                      <Button onClick={() => removeCartHandler(products[0].id)} text="Remove from Bag" innerSpacing="10px 8px" bgColor="#484848" color="#fff" opacity={0.4}/>
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
+            
           </div>
         </div>
       )}
